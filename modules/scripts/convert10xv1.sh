3cd735983bd640b472a26f44d8150b8e93b0337c
Vater Hu <92420346+WhatMelonGua@users.noreply.github.com>
2025-08-14 00:36:07 +0800

    Update convert10xv1.sh

--- modules/scripts/convert10xv1.sh
+++ modules/scripts/convert10xv1.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 <<README
-[convert10x_v1.sh] => 2025/03/01/-17:56	 # by huyw
-Intro:	来自Kimi生成, 参考xini姐代码, 转换10x 3' v1 数据
+[convert10x_v1.sh] => 2025/03/01/-17:56	 # by synnimeng & yiwen hu
+Intro:	convert 10x 3' v1 data.
 Usage:	bash convert10x_v1.sh -B <barcode.fq.gz> -R <read.fq.gz> -X <barcode+umi/r1> -O <read/r2>"
         # B: barcode, R: [read + umi]
 
 README
 
 
-# 定义帮助函数
+# Define help function
 usage() {
     echo "Usage: $0 -r1 <R1.fq.gz> -r2 <R2.fq.gz> -i1 <I1.fq.gz> -o <output_dir>"
     echo "Options:"
@@ -20,7 +20,7 @@ usage() {
     exit 1
 }
 
-# 解析命令行参数
+# Parse command-line arguments
 while getopts ":B:R:X:O:t::" opt; do
     echo $opt $OPTARG
     case ${opt} in
@@ -41,27 +41,25 @@ oR2=R2.fq.gz
 nThread=4
 EOF
 
-# 检查必要的参数是否已提供
+# Check if required arguments are provided
 if [[ -z "$inR1" || -z "$inR2" || -z "$oR1" || -z "$oR2" ]]; then
     echo "Error: Missing required arguments."
     usage
 fi
 
-# 创建输出目录
+# Create output directory
 mkdir -p "$(dirname $oR1)"
 mkdir -p "$(dirname $oR2)"
-# 临时文件夹
+# Temporary 
 tmpDir=$(dirname $oR1)
-
-# 定义输出文件路径
 tmpSID1="$tmpDir/SID1.txt"
 tmpSID2="$tmpDir/SID2.txt"
 tmpUMI="$tmpDir/UMI.fastq.gz"
 tmpAlignFlag="$tmpDir/isalign.flag"
 
-# 提取 UMI 和校验 reads 序列ID号
+# Extract UMI and verify read sequence IDs
 echo "10X 3' v1 prepare handling..."
-# 拆分R2 至 Read 和 UMI
+# Split R2 into Read and UMI
 zcat $inR2 | paste - - - - - - - - | \
     awk 'BEGIN{OFS="\n"} {print $1" "$2, $3, $4, $5}' | \
     gzip -c > "$oR2" &
@@ -71,25 +69,25 @@ zcat $inR2 | paste - - - - - - - - | \
     gzip -c > "$tmpUMI" &
 
 
-wait # 等待oR2 & UMI分发完毕
+wait # 
 seqkit seq -j $nThread "$inR1" -n -i > "$tmpSID1" &
 seqkit seq -j $nThread "$oR2" -n -i > "$tmpSID2" &
 
 
-# 合并 Cell Barcode 和 UMI
+# Concatenate Cell Barcode and UMI
 echo "Concatenating CB and UMI files..."
 zcat "$inR1" | paste - - - - <(zcat "$tmpUMI" | paste - - - -) | \
     awk 'BEGIN{OFS="\n"} {print $1" "$2, $3 $8, $4, $5 $10}' | \
     gzip -c > "$oR1"
 
-wait    # 等待统计SeqID 
-# 比较 ID 是否一致
+wait 
+# Compare whether IDs are identical
 if diff "$tmpSID1" "$tmpSID2" > /dev/null; then
     echo "IDs are identical."
-    echo "True" > "$tmpAlignFlag"   # ID  是否对齐
+    echo "True" > "$tmpAlignFlag"   # Whether IDs are aligned
 else
     echo "Warning: IDs are different!"
-    echo "False" > "$tmpAlignFlag"  # ID  是否对齐
+    echo "False" > "$tmpAlignFlag"  # Whether IDs are aligned
     exit 1
 fi
 
